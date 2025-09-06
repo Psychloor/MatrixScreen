d81239d0c38555e5d8c942a5ee659b22d7c8397a
blomq <blomqvistkevin@hotmail.com>
2025-09-06 19:28:51 +0200

    Refactor RNG seed logic: use `RANDOM_SEED_SIZE` and simplify seed generation with `std::ranges::generate`

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include <cstdlib>
 #include <iostream>
 #include <ostream>
-#include <vector>
 #include <random>
+#include <vector>
 
 #include "SDL3/SDL_main.h"
 
@@ -11,6 +11,8 @@
 #include "matrix_renderer.hpp"
 
 static constinit float MOUSE_THRESHOLD = 10.0F;
+// 16 should be random enough compared to 'std::mt19937::state_size' 624
+static constinit size_t RANDOM_SEED_SIZE = 16;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
@@ -63,8 +65,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
     double lastTime = static_cast<double>(SDL_GetPerformanceCounter());
 
     std::random_device rd;
-    std::vector<std::seed_seq::result_type> seeds(std::mt19937::state_size);
-    std::generate_n(std::begin(seeds), std::mt19937::state_size, std::ref(rd));
+    std::vector<std::seed_seq::result_type> seeds(RANDOM_SEED_SIZE);
+    std::ranges::generate(seeds, std::ref(rd));
     std::seed_seq seq(std::begin(seeds), std::end(seeds));
 
     std::mt19937 gen(seq);
